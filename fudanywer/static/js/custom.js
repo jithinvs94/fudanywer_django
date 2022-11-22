ee553bb442dddae0a1261b7747c2fc4243b43aae
Jithin Vs <jithinvsvks@gmail.com>
2022-11-23 02:20:44 +0530

    dynamic tax module

--- fudanywer/static/js/custom.js
+++ fudanywer/static/js/custom.js
@@ -98,7 +98,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
                 }
@@ -143,7 +143,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
                 }
@@ -172,7 +172,7 @@ $(document).ready(function(){
 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -202,20 +202,19 @@ $(document).ready(function(){
 
 
     // apply cart amounts
-    function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#total').html(grand_total)
 
-            // console.log(tax_dict)
-            // for(key1 in tax_dict){
-            //     console.log(tax_dict[key1])
-            //     for(key2 in tax_dict[key1]){
-            //         // console.log(tax_dict[key1][key2])
-            //         $('#tax-'+key1).html(tax_dict[key1][key2])
-            //     }
-            // }
+            console.log(tax_dict)
+            for(key1 in tax_dict){
+                console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    // console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
     }
 
