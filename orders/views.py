d61927406b6b2ce571bff12888fb9a07f4c58ae1
Jithin Vs <jithinvsvks@gmail.com>
2022-11-27 01:00:40 +0530

    razorpay payment implementation

--- orders/views.py
+++ orders/views.py
@@ -10,13 +10,13 @@ import simplejson as json
 from .utils import generate_order_number
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
-# import razorpay
-# from foodOnline_main.settings import RZP_KEY_ID, RZP_KEY_SECRET
+import razorpay
+from fudanywer.settings import RZP_KEY_ID, RZP_KEY_SECRET
 from django.contrib.sites.shortcuts import get_current_site
 
 
 
-# client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
+client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
 
 
 
@@ -91,24 +91,24 @@ def place_order(request):
             order.save()
 
             # RazorPay Payment
-            # DATA = {
-            #     "amount": float(order.total) * 100,
-            #     "currency": "INR",
-            #     "receipt": "receipt #"+order.order_number,
-            #     "notes": {
-            #         "key1": "value3",
-            #         "key2": "value2"
-            #     }
-            # }
-            # rzp_order = client.order.create(data=DATA)
-            # rzp_order_id = rzp_order['id']
+            DATA = {
+                "amount": float(order.total) * 100,
+                "currency": "INR",
+                "receipt": "receipt #"+order.order_number,
+                "notes": {
+                    "key1": "value3",
+                    "key2": "value2"
+                }
+            }
+            rzp_order = client.order.create(data=DATA)
+            rzp_order_id = rzp_order['id']
 
             context = {
                 'order': order,
                 'cart_items': cart_items,
-                # 'rzp_order_id': rzp_order_id,
-                # 'RZP_KEY_ID': RZP_KEY_ID,
-                # 'rzp_amount': float(order.total) * 100,
+                'rzp_order_id': rzp_order_id,
+                'RZP_KEY_ID': RZP_KEY_ID,
+                'rzp_amount': float(order.total) * 100,
             }
             return render(request, 'orders/place_order.html', context)
 
